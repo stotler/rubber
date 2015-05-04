d0bc96132df3cfda97c0379e368dd6222a66da03
Jim Salem <jsalem@backupify.com>
2015-05-04 11:27:35 -0400

    Create vagrant instance in create_instance call rather than before_create_instance. Consistent logging of vagrant commands.

--- lib/rubber/cloud/vagrant.rb
+++ lib/rubber/cloud/vagrant.rb
@@ -12,16 +12,19 @@ module Rubber
         'saved'
       end
 
-      def before_create_instance(instance_alias, role_names)
+      def create_instance(instance_alias, image_name, image_type, security_groups, availability_zone, region)
         unless ENV.has_key?('RUN_FROM_VAGRANT')
-          start_cmd = "vagrant up #{instance_alias}"
-          capistrano.logger.info("Starting instance using: #{start_cmd}")
-          system(start_cmd)
+          run_vagrant_command('up', instance_alias)
         end
+
+        instance = setup_vagrant_instance(instance_alias, active_state)
+        Generic.add_instance(instance)
+
+        instance_alias
       end
 
       def describe_instances(instance_id=nil)
-        output = `vagrant status #{instance_id}`
+        output = run_vagrant_command('status', instance_id, '', true)
         output =~ /#{instance_id}\s+(\w+)/m
         state = $1
 
@@ -35,13 +38,8 @@ module Rubber
 
           Generic.instances
         else
-          instance = {}
-          instance[:id] = instance_id
-          instance[:state] = state
-          instance[:external_ip] = capistrano.rubber.get_env('EXTERNAL_IP', "External IP address for host '#{instance_id}'", true)
-          instance[:internal_ip] = capistrano.rubber.get_env('INTERNAL_IP', "Internal IP address for host '#{instance_id}'", true, instance[:external_ip])
-          instance[:provider] = 'vagrant'
-
+          # Get info from currently running instance (or prompt user)
+          instance = setup_vagrant_instance(instance_id, state)
           [instance]
         end
       end
@@ -49,23 +47,74 @@ module Rubber
       def destroy_instance(instance_id)
         # If it's being run from vagrant, then 'vagrant destroy' must have been called already, so no need for us to do it.
         unless ENV.has_key?('RUN_FROM_VAGRANT')
-          cmd = "vagrant destroy #{instance_id} --force"
-          capistrano.logger.info("Destroying instance using: #{cmd}")
-          system(cmd)
+          run_vagrant_command('destroy', instance_id, '--force')
         end
       end
 
       def stop_instance(instance, force=false)
-        cmd = "vagrant suspend #{instance.instance_id}"
-        capistrano.logger.info("Stopping instance using: #{cmd}")
-        system(cmd)
+        run_vagrant_command('suspend', instance.instance_id)
       end
 
       def start_instance(instance)
-        cmd = "vagrant resume #{instance.instance_id}"
-        capistrano.logger.info("Starting existing instance using: #{cmd}")
-        system(cmd)
+       run_vagrant_command('resume', instance.instance_id)
       end
+
+      private
+
+      def run_vagrant_command(subcmd, instance_id = nil, args = "", return_result = false)
+        cmd = "vagrant #{subcmd} #{instance_id}"
+        cmd += " #{args}" unless args.empty?
+
+        capistrano.logger.info("Running '#{cmd}'")
+
+        if return_result
+          `#{cmd}`
+        else
+          system(cmd)
+        end
+      end
+
+      def setup_vagrant_instance(instance_alias, state)
+        instance = {}
+        instance[:id] = instance_alias
+        instance[:state] = state
+        instance[:provider] = 'vagrant'
+        instance[:platform] = Rubber::Platforms::LINUX
+        # IP addresses
+        ip = instance_external_ip(instance_alias)
+        if ! ip.empty?
+          capistrano.logger.info("Using #{ip} for external and internal IP address")
+          instance[:external_ip] = instance[:internal_ip] = ip
+        else
+          instance[:external_ip] = capistrano.rubber.get_env('EXTERNAL_IP', "External IP address for host '#{instance_alias}'", true)
+          instance[:internal_ip] = capistrano.rubber.get_env('INTERNAL_IP', "Internal IP address for host '#{instance_alias}'", true, instance[:external_ip])
+        end
+
+        instance
+      end
+
+      def instance_external_ip(instance_id)
+        return nil if instance_id.empty?
+        capistrano.logger.info("Getting Vagrant instance external IP")
+        ips = run_vagrant_command(instance_id, 'ssh', "-c 'ifconfig | awk \"/inet addr/{print substr(\\$2,6)}\"' 2> /dev/null", true)
+        ips = ips.split(/\r?\n/) # split on CRLF or LF
+        if ips.empty? 
+          capistrano.logger.error("Unable to retrieve IP addresses from Vagrant instance")
+          nil
+        else
+          original_ips = ips.dup
+          ips.delete_if { |x| /^127\./.match(x) }  # Delete the loopback address
+          ips.delete_if { |x| /^192\.168\.12/.match(x) }  # Delete the internally assigned Vagrant address: 192.168.12X.X
+          if ips.empty?
+            capistrano.logger.error("Vagrant instance doesn't appear to have an external IP address. IPs found are: #{original_ips.join(', ')}")
+            nil
+          else
+            capistrano.logger.info("The vagrant instance 'external' IP is #{ips.first}")
+            ips.first
+          end
+        end
+      end
+
     end
   end
 end
